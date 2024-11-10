92040b7a78ffd6d1d0152c7a09499a5f2de914c8
hamza03361 <hamzahakram160@gmail.com>
2024-11-10 14:12:16 +0500

    latestpush

---
+++ app/src/main/java/com/example/studentregistration/Apis/LoginData.kt
@@ -0,0 +1,35 @@
+package com.example.studentregistration.Apis
+
+import com.google.gson.annotations.SerializedName
+import java.io.File
+
+data class LoginData(
+    @SerializedName("reg_no") val reg_no: String,
+    @SerializedName("password") val userPassword: String,
+    @SerializedName("token") val token: String,
+)
+
+data class UploadFile(
+    val name: String,
+    val type: String,
+    val content: String
+)
+
+
+data class UploadResponse(
+    @SerializedName("First_name") val First_name: String,
+    @SerializedName("Last_name") val Last_name: String,
+    @SerializedName("Father_name") val Father_name: String,
+    @SerializedName("Email") val Email: String,
+    @SerializedName("Gender") val Gender: String,
+    @SerializedName("Date_of_birth") val Date_Of_birth: String,
+    @SerializedName("Registration_No") val Registration_No: String,
+    @SerializedName("Contact_No") val Contact_No: String,
+    @SerializedName("Semester") val Semester: String,
+    @SerializedName("Batch") val Batch: String,
+    @SerializedName("Address") val Address: String,
+    @SerializedName("Department") val department: String,
+    @SerializedName("Enrollment_year") val enrollmentyear: String,
+    @SerializedName("images") val images: List<UploadFile>,
+)
+
